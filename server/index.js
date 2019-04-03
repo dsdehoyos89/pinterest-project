a24af865b5ba01995dd737390b620f67ea5bb33b
Patric Khiev <webdev.patric@gmail.com>
2019-04-03 11:03:09 -0700

    server added

---
+++ server/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+// ** DEPENDENCIES **
+
+const express = require('express');
+const { json } = require('body-parser');
+const cors = require('cors');
+
+const port = process.env.PORT || 3001;
+
+const app = express();
+app.use(json());
+app.use(cors());
+
+app.listen(port, () => {
+  console.log(`Port listening on: ${port}`);
+});
